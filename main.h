cde730c6bba7d09adc5e3f2897ba71265f63c101
Bob <bohan612@gmail.com>
2015-08-03 16:32:15 -0700

    H file

--- main.h
+++ main.h
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAXLEN 1000
 
 typedef struct 
 {
-	char exprL, exprR[MAXLEN];	
+	char exprL[MAXLEN], exprR[MAXLEN];	
 	double lr, rr;
 	char op;
-	int lf, rf = 0;
+	int ld, rd;		//// left, right done.
+	int resOf;		//// Result of 0 -> left, 1 -> right.
 } Item;
 
 int mgetline(char s[], int lim);
 void push(Item i);
-Item pop();
+Item* pop();
 int isEmpty();
+Item* peek();
+int parseExpr(char expr[], int resOf);
+void print(char s[]);
+int height();
